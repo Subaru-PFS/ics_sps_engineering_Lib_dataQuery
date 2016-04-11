207fed76578ad7af866ca849bff2b09b8b05cd96
LE FUR Arnaud <arnaud.lefur@lam.fr>
2016-04-11 18:25:24 +0200

    network error fixed + bug utc fixed

--- databaseManager.py
+++ databaseManager.py
@@ -1,19 +1,21 @@
-from time import strptime, mktime
-import datetime
+import datetime as dt
 import csv
-
+import pytz
 import psycopg2
 from matplotlib.dates import date2num, num2date
 import numpy as np
+
 from astrotime import AstroTime
 
 
 class databaseManager():
     def __init__(self, ip, port):
 
-        self.error_code = {-1: "network_database", -2: "bad_request", -3: "bad date format", -4: "no_data"}
+        self.error_code = {-1: "network_database", -2: "bad_request", -3: "bad date format", -4: "no_data",
+                           -5: "network disconnected"}
         self.ip = ip
         self.port = port
+        self.reconnecting = False
 
     def initDatabase(self):
         try:
@@ -25,8 +27,40 @@ class databaseManager():
         except psycopg2.OperationalError:
             return -1
 
-    def getData(self, tableName, keyword, id_inf=0, id_sup="Now", convert=True):
+    def reconnectDatabase(self):
+        if not self.reconnecting:
+            self.reconnecting = True
+            err = self.initDatabase()
+            self.reconnecting = False
+        else:
+            pass
 
+    def getrowrelative2Date(self, tableName, keyword, date_num, force=False):
+        if date_num is not -1:
+            request = """select %s from reply_raw inner join %s on %s.raw_id=reply_raw.id WHERE tai > %f order by tai asc limit 1""" % (
+                keyword, tableName, tableName, date_num)
+        else:
+            request = """select %s from reply_raw inner join %s on %s.raw_id=reply_raw.id order by raw_id desc limit 1 """ % (
+                keyword, tableName, tableName)
+        try:
+            self.database.execute(request)
+        except psycopg2.ProgrammingError:
+            self.conn.rollback()
+            return -2
+        except (psycopg2.InterfaceError, psycopg2.DatabaseError) as e:
+            self.reconnectDatabase()
+            return -5
+        try:
+            [(row_result,)] = self.database.fetchall()
+            return row_result
+        except ValueError:
+            if not force:
+                return -4
+            else:
+                return self.getrowrelative2Date(tableName, keyword, -1)
+
+    def getData(self, tableName, keyword, id_inf=0, id_sup="Now", convert=True):
+        t0 = dt.datetime.now()
         request = """select id, tai, %s from reply_raw inner join %s on %s.raw_id=reply_raw.id where (%s.raw_id>%i) order by id asc""" % (
             keyword, tableName, tableName, tableName,
             id_inf) if id_sup == "Now" else  """select id, tai, %s from reply_raw inner join %s on %s.raw_id=reply_raw.id where (%s.raw_id>%i and %s.raw_id<=%i) order by id asc""" % (
@@ -36,6 +70,9 @@ class databaseManager():
         except psycopg2.ProgrammingError:
             self.conn.rollback()
             return id_inf, -2, -2
+        except (psycopg2.InterfaceError, psycopg2.DatabaseError) as e:
+            self.reconnectDatabase()
+            return id_inf, -5, -5
         all_data = self.database.fetchall()
         if all_data:
             data_id = np.zeros(len(all_data))
@@ -46,6 +83,7 @@ class databaseManager():
                 data_date[i] = self.convertfromAstro(data[1]) if convert else data[1]
                 data_values[i] = data[2:]
             if convert:
+                print (dt.datetime.now()-t0).total_seconds()
                 return data_id[-1], data_date, data_values
             else:
                 return data_id, data_date, data_values
@@ -62,6 +100,9 @@ class databaseManager():
         except psycopg2.ProgrammingError:
             self.conn.rollback()
             return -2, -2
+        except (psycopg2.InterfaceError, psycopg2.DatabaseError) as e:
+            self.reconnectDatabase()
+            return -5, -5
         data = self.database.fetchall()
         if data:
             date = self.convertfromAstro(data[0][0])
@@ -122,27 +163,6 @@ class databaseManager():
         else:
             return None
 
-    def getrowrelative2Date(self, tableName, keyword, date_num, force=False):
-        if date_num != -1:
-            request = """select %s from reply_raw inner join %s on %s.raw_id=reply_raw.id WHERE tai > %f order by tai asc limit 1""" % (
-                keyword, tableName, tableName, date_num)
-        else:
-            request = """select %s from reply_raw inner join %s on %s.raw_id=reply_raw.id order by raw_id desc limit 1 """ % (
-                keyword, tableName, tableName)
-        try:
-            self.database.execute(request)
-        except psycopg2.ProgrammingError:
-            self.conn.rollback()
-            return -2
-        try:
-            [(row_result,)] = self.database.fetchall()
-            return row_result
-        except ValueError:
-            if not force:
-                return -4
-            else:
-                return self.getrowrelative2Date(tableName, keyword, -1)
-
     def getDatafromDate(self, tableName, keyword, date):
         id = self.getrowrelative2Date(tableName, "id", self.convertTimetoAstro(date))
         if id not in [-1, -2, -3, -4]:
@@ -171,9 +191,9 @@ class databaseManager():
             return id, id
 
     def convertTimetoAstro(self, date):
-        date_num = datetime.datetime.fromtimestamp(mktime(strptime(date, "%d/%m/%Y %H:%M:%S")))
-        date_num = (date_num - datetime.datetime(1970, 1, 1)).total_seconds()
-        date_num = AstroTime.fromtimestamp(date_num)
+        date_num = dt.datetime.strptime(date, "%d/%m/%Y %H:%M:%S")
+        date_num = (date_num - dt.datetime(1970, 1, 1)).total_seconds()
+        date_num = AstroTime.fromtimestamp(date_num, tz = pytz.utc)
         date_num = date_num.MJD() * 86400
         return date_num
 
@@ -182,11 +202,11 @@ class databaseManager():
         date = AstroTime.fromMJD(date / 86400)
         date = str(date).split("MJD")[0][:-1]
         try:
-            datenum = date2num(datetime.datetime.fromtimestamp(mktime(strptime(date, "%Y-%m-%d %H:%M:%S.%f"))))
+            datenum = date2num(dt.datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f"))
             return datenum
         except ValueError:
             try:
-                datenum = date2num(datetime.datetime.fromtimestamp(mktime(strptime(date, "%Y-%m-%d %H:%M:%S"))))
+                datenum = date2num(dt.datetime.strptime(date, "%Y-%m-%d %H:%M:%S"))
                 return datenum
             except ValueError:
                 return 0
